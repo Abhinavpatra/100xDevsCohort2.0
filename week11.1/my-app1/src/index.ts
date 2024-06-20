2e0c8ff07749eba372f75e29ef779a0b3fea74b3
Abhinavpatra <149466436+Abhinavpatra@users.noreply.github.com>
2024-06-21 00:01:45 +0530

    almost over

--- week11.1/my-app1/src/index.ts
+++ week11.1/my-app1/src/index.ts
@@ -3,12 +3,16 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', async (c) => {
-  const body = await c.req.json()
+
+  return c.text('Hello Hono!fnn')
+})
+
+app.post('/', async (c)=>{
+
+  const body=await c.req.json()
   console.log(body);
   console.log(c.req.header("Authorization"));
   console.log(c.req.query("param"));
-
-  return c.text('Hello Hono!')
+  return c.text('Hellp Hono!')
 })
-
 export default app
